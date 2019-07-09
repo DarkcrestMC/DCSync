936f3f3a0c3e6f1db0539200a56ccd8b3e415688
Jacksonnn <jhuse01@gmail.com>
2019-07-09 00:50:04 -0400

    Add files via upload

---
+++ src/main/java/net/DarkcrestMC/DCSync/DCSync.java
@@ -0,0 +1,17 @@
+package net.DarkcrestMC.DCSync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DCSync extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
